4b2b558c8cb67bea52b2483b098df12126a8442b
ayaz1017 <your-email@example.com>
2025-08-17 16:17:28 +0530

    New

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/restaurant', (req, res) => {
 
 setInterval(() => {
   axios.get("https://food-delivery-2un8.onrender.com/");
-}, 1000 * 60 * 15);
+}, 1000 * 60 * 13);
 
 
 const PORT = process.env.PORT || 4000;
